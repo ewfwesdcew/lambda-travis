09852cd8e1a6b5a2e995be1a2af021ffdf449e3f
Aabhusan <aabhusan@pop-os.localdomain>
2020-04-23 22:06:27 +0545

    new files added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT ? process.env.PORT : 3000;
 
-app.get("/", (req, res) => res.send("Hello World! from production"));
+app.get("/", (req, res) => res.send("Hello World! from Devops tutorials."));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
